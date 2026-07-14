171c1add06bc6faf9d8c3e5acca9e871ca0261ea
munteanudenis87 <munteanudenis87@gmail.com>
2026-07-14 21:31:14 +0200

    Milestone 2: Implementare la ricerca ottimizzata

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useMemo } from "react"
 
 function App() {
 
   const [politici, setPolitici] = useState([]);
+
+  const [cerca, setCerca] = useState('');
 
   useEffect(() => {
     fetch("http://localhost:3333/politicians")
@@ -13,12 +15,26 @@ function App() {
 
   console.log(politici);
 
+  const filtraPolitici = useMemo(() => {
+    return politici.filter(politico => {
+      const nomePolitico = politico.name.toLowerCase().includes(cerca.toLowerCase());
+      const biographyPolitico = politico.biography.toLowerCase().includes(cerca.toLowerCase());
+      return nomePolitico || biographyPolitico;
+    });
+  }, [politici, cerca]);
+
   return (
     <>
       <div>
+        <input 
+          type="text"
+          placeholder="Cerca politico"
+          value={cerca}
+          onChange={event => setCerca(event.target.value)}
+        />
         <h2>Lista Politici</h2>
         <div>
-          {politici.map(politico => (
+          {filtraPolitici.map(politico => (
             <div className="card" key={politico.id}>
               <div><img className="img-politico" src={politico.image} alt={politico.name} /></div>
               <div>
@@ -54,3 +70,10 @@ export default App
 //  Breve biografia (biography)
 
 //  Obiettivo: Caricare e mostrare i politici in un’interfaccia chiara e leggibile.
+
+//  Milestone 2: Implementare la ricerca ottimizzata
+//  Aggiungi un campo di ricerca (<input type="text">) sopra la lista dei politici.
+//  Permetti all’utente di filtrare i risultati in base a nome o biografia (se il testo cercato è incluso). Suggerimento: Creare un array derivato filtrato, che viene aggiornato solo quando cambia la lista di politici o il valore della ricerca.
+//  ❌ Non usare useEffect per aggiornare l’array filtrato.
+
+//  Obiettivo: Migliorare le prestazioni evitando ricalcoli inutili quando il valore della ricerca non cambia.
